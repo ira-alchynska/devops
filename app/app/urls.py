518b3a0f34bd7b30e9b23526cda07b660bf73f83
Iryna Alchynska <68770490+ira-alchynska@users.noreply.github.com>
2025-11-15 14:27:05 +0100

    add simple hello world app

--- app/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.http import HttpResponse
-
-urlpatterns = [
-    path('', lambda r: HttpResponse("Hello, world! This is Django on EKS 🚀")),
-    path('hello/', include('hello.urls')),
-    path('healthz/', lambda r: HttpResponse("ok")),
-]
--- app/hello/urls.py
+++ app/hello/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.hello_world, name='hello_world'),
 ]
+
