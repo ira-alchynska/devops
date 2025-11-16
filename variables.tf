ad19d4021f958877ab89057944b83784e8f05a2c
Iryna Alchynska <68770490+ira-alchynska@users.noreply.github.com>
2025-11-16 14:53:14 +0100

    initial setup for lesson 8-9

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "region" {
+  description = "AWS region for deployment"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "eks-cluster-alex"
+}
+
+variable "vpc_name" {
+  default = "vpc-alex"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for the worker nodes"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "repository_name" {
+  description = "Name of the ECR repository"
+  type        = string
+  default     = "ecr-repo-18062025214500"
+}
+
+variable "github_pat" {
+  description = "GitHub Personal Access Token"
+  type        = string
+}
+
+variable "github_user" {
+  description = "GitHub username"
+  type        = string
+}
+
+variable "github_repo_url" {
+  description = "GitHub repository name"
+  type        = string
+}
